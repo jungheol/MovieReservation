af7b6ae3757a1b26565d4ca5317f0414fe9a8a10
jungheol <jungheol@naver.com>
2024-08-23 18:50:05 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/test/java/com/zerobase/moviereservation/MoviereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.moviereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
