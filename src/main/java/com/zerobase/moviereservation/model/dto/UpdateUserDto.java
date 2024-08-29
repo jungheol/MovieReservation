dd1c3dda883dc52b1b18605872a93bdc45fb8697
jungheol <jungheol@naver.com>
2024-08-30 00:19:10 +0900

    feat: UpdateUserDto 추가

---
+++ src/main/java/com/zerobase/moviereservation/model/dto/UpdateUserDto.java
@@ -0,0 +1,42 @@
+package com.zerobase.moviereservation.model.dto;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class UpdateUserDto {
+
+  @Data
+  @Builder
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Request {
+    private String email;
+    private String password;
+    private String username;
+    private LocalDate birthday;
+    private String phoneNumber;
+  }
+
+  @Data
+  @Builder
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Response {
+    private String password;
+    private String username;
+    private LocalDate birthday;
+    private String phoneNumber;
+
+    public static Response from(UserDto userDto) {
+      return Response.builder()
+          .password(userDto.getPassword())
+          .username(userDto.getUsername())
+          .birthday(userDto.getBirthday())
+          .phoneNumber(userDto.getPhoneNumber())
+          .build();
+    }
+  }
+}
