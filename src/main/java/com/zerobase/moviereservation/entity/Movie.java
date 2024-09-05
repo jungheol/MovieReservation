a40f31bae220dc4895eebce85112543cc65b7923
jungheol <jungheol@naver.com>
2024-09-05 18:25:38 +0900

    feat: Movie Entity 추가

---
+++ src/main/java/com/zerobase/moviereservation/entity/Movie.java
@@ -0,0 +1,40 @@
+package com.zerobase.moviereservation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "movie")
+public class Movie extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+
+  private String director;
+
+  private String genre;
+
+  private String runningTime;
+
+  private LocalDate releaseDate;
+
+  private Integer rating;
+
+}
