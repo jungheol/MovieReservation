63f6c7856b59198124c5382fec3554717fbed173
jungheol <jungheol@naver.com>
2024-09-05 23:42:45 +0900

    feat: schedule entity 추가

---
+++ src/main/java/com/zerobase/moviereservation/entity/Schedule.java
@@ -0,0 +1,40 @@
+package com.zerobase.moviereservation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "schedule")
+public class Schedule {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "movie_id")
+  private Movie movie;
+
+  @ManyToOne
+  @JoinColumn(name = "theater_id")
+  private Theater theater;
+
+  private LocalTime startTime;
+
+}
