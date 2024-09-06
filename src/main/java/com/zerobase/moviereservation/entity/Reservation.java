6250fefd555e939ee24580a2d27be00ef6bc5af5
jungheol <jungheol@naver.com>
2024-09-06 22:06:05 +0900

    feat: reservation entity, repository 추가

---
+++ src/main/java/com/zerobase/moviereservation/entity/Reservation.java
@@ -0,0 +1,55 @@
+package com.zerobase.moviereservation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reservation")
+@EntityListeners(AuditingEntityListener.class)
+public class Reservation {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "schedule_id")
+  private Schedule schedule;
+
+  @ManyToOne
+  @JoinColumn(name = "seat_id")
+  private Seat seat;
+
+  private String cancel;
+
+  private String reserved;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  private LocalDateTime canceledAt;
+}
---
+++ src/main/java/com/zerobase/moviereservation/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.zerobase.moviereservation.repository;
+
+import com.zerobase.moviereservation.entity.Reservation;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+  List<Reservation> findByScheduleIdAndSeatIdIn(Long scheduleId, List<Long> seatIds);
+}
