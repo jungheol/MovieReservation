bf6b2c3deee9028fef73a47bdc130fbda08c0261
jungheol <jungheol@naver.com>
2024-09-11 15:58:51 +0900

    feat: review entity 추가

---
+++ src/main/java/com/zerobase/moviereservation/entity/Review.java
@@ -0,0 +1,43 @@
+package com.zerobase.moviereservation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "review")
+public class Review extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "reservation_id")
+  private Reservation reservation;
+
+  @Column(length = 250)
+  private String content;
+
+  private Integer rating;
+
+}
