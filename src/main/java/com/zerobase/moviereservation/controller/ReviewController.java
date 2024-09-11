79564d7ec3c43590478d9484ef75c605fefdf9db
jungheol <jungheol@naver.com>
2024-09-11 16:00:44 +0900

    feat: review controller 및 requestMatchers 추가

---
+++ src/main/java/com/zerobase/moviereservation/controller/ReviewController.java
@@ -0,0 +1,52 @@
+package com.zerobase.moviereservation.controller;
+
+import com.zerobase.moviereservation.model.dto.RegisterReviewDto;
+import com.zerobase.moviereservation.model.dto.UpdateReviewDto;
+import com.zerobase.moviereservation.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+  private final ReviewService reviewService;
+
+  @PostMapping
+  public RegisterReviewDto.Response registerReview(
+      @RequestParam("userId") Long userId,
+      @RequestParam("reservationId") Long reservationId,
+      @RequestBody RegisterReviewDto.Request request
+  ) {
+    return RegisterReviewDto.Response.from(
+        this.reviewService.registerReview(userId, reservationId, request));
+  }
+
+  @PatchMapping("/{reviewId}")
+  public UpdateReviewDto.Response updateReview(
+      @RequestParam("userId") Long userId,
+      @PathVariable("reviewId") Long reviewId,
+      @RequestBody UpdateReviewDto.Request request
+  ) {
+    return UpdateReviewDto.Response.from(
+        this.reviewService.updateReview(userId, reviewId, request));
+  }
+
+  @DeleteMapping("/{reviewId}")
+  public ResponseEntity<?> deleteReview(
+      @RequestParam("userId") Long userId,
+      @PathVariable("reviewId") Long reviewId
+  ) {
+    this.reviewService.deleteReview(userId, reviewId);
+    return ResponseEntity.ok("해당 리뷰를 삭제하였습니다.");
+  }
+}
