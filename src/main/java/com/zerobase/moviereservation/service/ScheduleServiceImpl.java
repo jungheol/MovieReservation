fb1dfd184c08947d7fa8e5c84153483980ea5dab
jungheol <jungheol@naver.com>
2024-09-12 17:42:41 +0900

    feat: schedule lock aop 적용

--- src/main/java/com/zerobase/moviereservation/service/ScheduleServiceImpl.java
+++ src/main/java/com/zerobase/moviereservation/service/ScheduleServiceImpl.java
@@ -3,6 +3,7 @@ package com.zerobase.moviereservation.service;
 import static com.zerobase.moviereservation.exception.type.ErrorCode.ALREADY_EXISTED_SCHEDULE;
 import static com.zerobase.moviereservation.exception.type.ErrorCode.SCHEDULE_NOT_FOUND;
 
+import com.zerobase.moviereservation.aop.RedisLock;
 import com.zerobase.moviereservation.entity.Movie;
 import com.zerobase.moviereservation.entity.Schedule;
 import com.zerobase.moviereservation.entity.Theater;
@@ -30,6 +31,7 @@ public class ScheduleServiceImpl implements ScheduleService {
 
   @Override
   @Transactional
+  @RedisLock(keys = "schedule_lock:#request.theaterId:#request.startTime")
   public ScheduleDto registerSchedule(Request request) {
     Movie movie = this.movieRepository.findById(request.getMovieId())
         .orElseThrow(() -> new CustomException(ErrorCode.MOVIE_NOT_FOUND));
@@ -37,7 +39,6 @@ public class ScheduleServiceImpl implements ScheduleService {
     Theater theater = this.theaterRepository.findById(request.getTheaterId())
         .orElseThrow(() -> new CustomException(ErrorCode.THEATER_NOT_FOUND));
 
-    // 추후에 lock 으로 동시에 스케쥴이 잡히지 않도록 구현 추가
     if (this.scheduleRepository.existsByTheaterIdAndStartTime(
         theater.getId(), request.getStartTime())) {
       throw new CustomException(ALREADY_EXISTED_SCHEDULE);
